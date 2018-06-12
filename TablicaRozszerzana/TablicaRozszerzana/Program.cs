b3f3e1de04b1a1f55355c696c6568d14c2c4ee3e
Famulski.Michal <187684@edu.p.lodz.pl>
2018-06-12 23:49:54 +0200

    added event handling

--- TablicaRozszerzana/TablicaRozszerzana/Program.cs
+++ TablicaRozszerzana/TablicaRozszerzana/Program.cs
@@ -6,44 +6,35 @@ using System.Threading.Tasks;
 
 namespace TablicaRozszerzana
 {
-    class Program
+    public class TableResizedEventArgs : EventArgs
     {
-        public static void WriteTable(Tablica tab)
+        private int initialSize;
+        private int currentSize;
+        private string description;
+
+        public TableResizedEventArgs(int initialSize, int currentSize)
         {
-            Console.WriteLine("Size " + tab.Size);
-            for (int i = 0; i < tab.Size; i++)
-            {
-                Console.WriteLine("Index " + i + ": " + tab[i]);
-            }
+            this.initialSize = initialSize;
+            this.currentSize = currentSize;
+            this.description = "Table resized from initial size of " + initialSize + " to current size of " + currentSize;
         }
-        static void Main(string[] args)
+
+        public int InitialSize
         {
-            Tablica tab = new Tablica();
-           
-            tab.Add(7);
-            tab.Add(-5);
-            tab.AddAt(-100, 0);
-            tab.Add(2);
-            tab.AddAt(-111, 7);
-            tab[14] = -90;
-            WriteTable(tab);
-            int val, index;
-            do
-            {
-                Console.WriteLine("Podaj index: ");
-                index = int.Parse(Console.ReadLine());
-                Console.WriteLine("Podaj wartosc: ");
-                val = int.Parse(Console.ReadLine());
-                if(index != 0 && val != 0)
-                {
-                    tab[index] = val;
-                }
-            } while (index !=0 && val != 0);
-            WriteTable(tab);
-            Console.ReadKey();
+            get { return this.initialSize; }
         }
- 
+        public int CurrentSize
+        {
+            get { return this.currentSize; }
+        }
+        public string Description
+        {
+            get { return this.description; }
+        }
     }
+
+    public delegate void TableResizedEventHandler(object sender, TableResizedEventArgs e);
+
     class Tablica
     {
         private int[] table;
@@ -51,13 +42,17 @@ namespace TablicaRozszerzana
         private int allocatedSize;
         private int arrayExpandFactor;
         private int defaultValue;
-
+        public event TableResizedEventHandler TableResized;
+        protected virtual void onTableResized(TableResizedEventArgs e)
+        {
+            if (TableResized != null)
+            {
+                TableResized(this, e);
+            }
+        }
         public int Size
         {
-            get
-            {
-                return size;
-            }
+            get { return size; }
         }
         public int this[int index]
         {
@@ -72,70 +67,127 @@ namespace TablicaRozszerzana
                     throw new IndexOutOfRangeException();
                 }
             }
-            set
-            {
-                AddAt(value, index);
-            }
+            set { AddAt(index, value); }
         }
-        public Tablica(int size)
+        public Tablica(int initialTableSize)
         {
             size = 0;
             arrayExpandFactor = 2;
             defaultValue = -1;
-            if (size > 4)
+            if (initialTableSize > 4)
             {
-                allocatedSize = size * arrayExpandFactor;
-
-            } else
+                allocatedSize = initialTableSize * arrayExpandFactor;
+            }
+            else
             {
                 allocatedSize = 4 * arrayExpandFactor;
             }
-            table = new int[allocatedSize];       
-
+            table = new int[allocatedSize];
         }
 
 
-        public Tablica():this(0)
+        public Tablica() : this(0)
         {
         }
 
-        public void Resize() //Zrobic resize do index * 2, zamiast resize'owac wielokrotnie np dla 1000 indexu
+        public void Resize(int targetSize) //Zrobic resize do index * 2, zamiast resize'owac wielokrotnie np dla 1000 indexu
         {
-            int expandBase = size;
-            int newAllocatedSize = expandBase * arrayExpandFactor;
-            int[] newTable = new int[newAllocatedSize];
-            table.CopyTo(newTable, 0);
-            table = newTable;
-            allocatedSize = newAllocatedSize;
+            if (targetSize >= allocatedSize)
+            {
+                int newAllocatedSize = targetSize * arrayExpandFactor;
+                TableResizedEventArgs eventArgs = new TableResizedEventArgs(allocatedSize, newAllocatedSize);
+                int[] newTable = new int[newAllocatedSize];
+                table.CopyTo(newTable, 0);
+                table = newTable;
+                allocatedSize = newAllocatedSize;
+                onTableResized(eventArgs);
+                for (int i = size; i < allocatedSize; i++)
+                {
+                    table[i] = defaultValue;
+                }
+            }
+
         }
         public void Add(int value)
         {
-            if(size >= allocatedSize)
-            {
-                Resize();
-            }      
+            Resize(size);
             table[size] = value;
             size++;
         }
-        public void AddAt(int value, int index)
+        public void AddAt(int index, int value)
         {
-            if(index == size)
+            if (index >= 0)
             {
-                Add(value);
-            } else if (index > size)
-            {
-                for(int i = size; i < index; i++)
-                {
-                    Add(defaultValue);
-                }
-                Add(value);
-            } else if(index >=0)
-            {
+                Resize(index);
                 table[index] = value;
-            } else
+
+                if (index >= size)
+                {
+                    size = index + 1;
+                }
+            }
+            else
             {
                 throw new IndexOutOfRangeException();
             }
         }
+    }
+
+    class Program
+    {
+        static void TableResized(object source, TableResizedEventArgs e)
+        {
+            Console.WriteLine(e.Description);
+        }
+        public static void WriteTable(Tablica tab)
+        {
+            Console.WriteLine("Size " + tab.Size);
+            for (int i = 0; i < tab.Size; i++)
+            {
+                Console.WriteLine("Index " + i + ": " + tab[i]);
+            }
+        }
+        public static void WriteTableWithoutDefaults(Tablica tab)
+        {
+            Console.WriteLine("Size " + tab.Size);
+            for (int i = 0; i < tab.Size; i++)
+            {
+                if (tab[i] != -1)
+                {
+                    Console.WriteLine("Index " + i + ": " + tab[i]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Tablica tab = new Tablica();
+            //tab.TableResized += new TableResizedEventHandler(TableResized);
+            tab.TableResized += TableResized;
+
+            tab.Add(7);
+            tab.Add(-5);
+            tab.AddAt(0, -5);
+            tab.Add(2);
+            tab.AddAt(7, -45);
+            tab[9] = -90;
+            tab.AddAt(19, -45);
+            WriteTable(tab);
+            int val, index;
+            do
+            {
+                Console.WriteLine("Podaj index: ");
+                index = int.Parse(Console.ReadLine());
+                Console.WriteLine("Podaj wartosc: ");
+                val = int.Parse(Console.ReadLine());
+                if (index != 0 && val != 0)
+                {
+                    tab[index] = val;
+                }
+                WriteTableWithoutDefaults(tab);
+            } while (index != 0 && val != 0);
+            WriteTable(tab);
+            Console.ReadKey();
+        }
+
     }
 }
